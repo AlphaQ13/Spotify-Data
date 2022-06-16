380c61caa5ee61839c98bfdf389fbd6b92db554d
AlphaQ13 <64019184+AlphaQ13@users.noreply.github.com>
2022-06-16 18:48:09 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ var SPOTIFY_CLIENT_ID = '0477e88f92c641fda1fb6e5fdbe5af37'
 //var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
 //var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
 //var SPOTIFY_REDIRECT_URI = 'http://sortyourmusic.playlistmachinery.com/';
-var SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5500/web/index.html'
+//var SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5500/web/index.html'
+var SPOTIFY_REDIRECT_URI = 'https://alphaq13.github.io/Spotify-Data/'
