6765e763a8aa83b4c8fcc0ab0e50e1a601eae1c6
AlphaQ13 <64019184+AlphaQ13@users.noreply.github.com>
2022-06-16 18:38:59 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//var SPOTIFY_CLIENT_ID = '31469b011d4941bf8dd4ac9cf8495bac';
+var SPOTIFY_CLIENT_ID = '0477e88f92c641fda1fb6e5fdbe5af37'
+//var SPOTIFY_REDIRECT_URI = 'http://localhost:8235/SortYourMusic/';
+//var SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
+//var SPOTIFY_REDIRECT_URI = 'http://sortyourmusic.playlistmachinery.com/';
+var SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5500/web/index.html'
